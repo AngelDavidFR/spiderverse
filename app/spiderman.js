8c89912cbc0cfa9b2c519da361ce5e39316111a0
Angel David Flores Romero <angeldavidfr12@gmail.com>
2022-04-23 17:06:56 -0500

    Refactor y requerimiento final

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,3 +1,17 @@
-class Spiderman{}
+class Spiderman{
+constructor(name,age,actor,movies,studio){
+    this.name=name
+    this.age=age
+    this.actor=actor
+    this.movies=movies
+    this.studio=studio
+    
+}
+
+getinfo(){
+    return `Hey, I'm ${this.actor} from ${this.studio}`
+}
+
+}
 
 module.exports=Spiderman
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,10 +1,17 @@
+const Spiderman=require('./../app/spiderman')
 describe("Unit test for Spiderman class", () => {
     test('1) Create an spiderman object', () => {
-      const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,31,"Sony")
+      const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
       expect(andrewGarfield.name).toBe("Spiderman Sony")
       expect(andrewGarfield.age).toBe(31)
       expect(andrewGarfield.actor).toBe("Andrew Garfield")
-      expect(andrewGarfield.movies).toBe(31)
+      expect(andrewGarfield.movies).toBe(2)
       expect(andrewGarfield.studio).toBe("Sony")
     });
+    test('2) Use the method getinfo()',()=>{
+      const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+      expect(andrewGarfield.getinfo()).toBe("Hey, I'm Andrew Garfield from Sony")
+
+
+    })
   })
